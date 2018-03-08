ea879f82b0d2e980886abf56a7d4be717849ec9a
Mathew Robinson <chasinglogic@gmail.com>
2018-03-08 11:53:37 -0500

    Move cache logic into find_projects so all commands can take advantage

--- src/main.rs
+++ src/main.rs
@@ -39,12 +39,69 @@ fn handle_error(e: walkdir::Error) -> bool {
     return false;
 }
 
-fn find_projects<F>(code_dir: String, callback: F)
+fn should_use_cache(cache_file: &Path) -> bool {
+    // Obviously if there's no cache we shouldn't use it.
+    if !cache_file.exists() {
+        return false;
+    }
+
+    let cache_file_md = metadata(cache_file);
+
+    match cache_file_md {
+        Ok(md) => {
+            let last_cache_access;
+            if let Ok(time) = md.modified() {
+                last_cache_access = time.elapsed().unwrap_or(Duration::new(0, 0));
+                // 172800 is 48 hours in seconds. Rust doesn't have a better
+                // way to work with time.
+                return last_cache_access.as_secs() < 172800;
+            }
+
+            true
+        }
+        Err(_) => false,
+    }
+}
+
+
+fn find_projects<F>(code_dir: String, ignore_cache: bool, callback: F)
 where
-    F: Fn(String) -> (),
+    F: Fn(&str) -> (),
+{
+    let mut home = env::var("HOME").unwrap_or("".to_string());
+    home.push_str("/.projector_cache");
+    let cache_file = Path::new(&home);
+
+    let use_cache = !ignore_cache && should_use_cache(&cache_file);
+
+    if use_cache {
+        find_projects_from_cache(&cache_file, callback);
+    } else {
+        find_projects_from_fs(code_dir, &cache_file, callback);
+    }
+}
+
+fn find_projects_from_cache<F>(cache_file: &Path, callback: F)
+where
+    F: Fn(&str) -> (),
+{
+    let mut f = File::open(cache_file).expect("Cache file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Unable to read from cache file.");
+
+    for p in contents.as_str().split("\n") {
+        callback(p);
+    }
+}
+
+fn find_projects_from_fs<F>(code_dir: String, cache_file: &Path, callback: F)
+where
+    F: Fn(&str) -> (),
 {
     let wkd = WalkDir::new(code_dir);
     let git_dirs = wkd.into_iter();
+    let mut projects = Vec::new();
 
     for dir in git_dirs {
         let cwd;
@@ -69,47 +126,14 @@ where
                 .parent()
                 .unwrap()
                 .to_str()
-                .unwrap_or("")
-                .to_string();
+                .unwrap_or("");
 
-            callback(parent_path);
+            callback(&parent_path);
+            projects.push(parent_path.to_string().clone());
         }
     }
-}
 
-fn run(code_dir: String, command: Vec<String>) {
-    if let Some((program, arguments)) = command.split_first() {
-        find_projects(code_dir, |p| {
-            let mut child = Command::new(program)
-                .args(arguments)
-                .current_dir(p)
-                .spawn()
-                .expect("failed to start process");
-            child.wait().expect("failed to execute child process");
-            ()
-        })
-    } else if command.len() == 0 {
-        println!("ERROR: No command given");
-    }
-}
-
-fn list_from_cache(cache_file: &Path) {
-    let mut f = File::open(&cache_file).expect("Cache file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Unable to read from cache file.");
-    println!("{}", contents)
-}
-
-fn list_from_fs(code_dir: String, cache_file: &Path) {
-    let projects = Arc::new(Mutex::new(Vec::new()));
-
-    find_projects(code_dir, |p| {
-        println!("{}", p);
-        projects.lock().unwrap().push(p.clone())
-    });
-
-    let results = projects.lock().unwrap().join("\n");
+    let results = projects.join("\n");
 
     let mut f = OpenOptions::new()
         .create(true)
@@ -124,41 +148,35 @@ fn list_from_fs(code_dir: String, cache_file: &Path) {
     }
 }
 
-fn should_use_cache(cache_file: &Path) -> bool {
-    // Obviously if there's no cache we shouldn't use it.
-    if !cache_file.exists() {
-        return false
-    }
+fn run(code_dir: String, ignore_cache: bool, command: Vec<String>) {
+    if let Some((program, arguments)) = command.split_first() {
+        find_projects(code_dir, ignore_cache, |p| {
+            println!("\n{}", "=".repeat(80));
+            println!("Running in: {}", p);
+            println!("{}\n", "=".repeat(80));
+            let child = Command::new(program)
+                .args(arguments)
+                .current_dir(p)
+                .spawn();
 
-    let cache_file_md = metadata(cache_file);
-
-    match cache_file_md {
-        Ok(md) => {
-            let last_cache_access;
-            if let Ok(time) = md.modified() {
-                last_cache_access = time.elapsed().unwrap_or(Duration::new(0, 0));
-                // 172800 is 48 hours in seconds. Rust doesn't have a better 
-                // way to work with time.
-                return last_cache_access.as_secs() < 172800;
+            match child {
+                Ok(mut subproc) => {
+                    if let Err(e) = subproc.wait() {
+                        println!("{}", e);
+                    }
+                },
+                Err(e) => println!("Error spawning process: {}", e),
             }
-
-            true
-        }
-        Err(_) => false,
+        })
+    } else if command.len() == 0 {
+        println!("ERROR: No command given");
     }
 }
 
 fn list(code_dir: String, ignore_cache: bool) {
-    let mut home = env::var("HOME").unwrap_or("".to_string());
-    home.push_str("/.projector_cache");
-    let cache_file = Path::new(&home);
-
-    let use_cache = !ignore_cache && should_use_cache(&cache_file);
-    if use_cache {
-        list_from_cache(&cache_file)
-    } else {
-        list_from_fs(code_dir, cache_file)
-    }
+    find_projects(code_dir, ignore_cache, |p| {
+        println!("{}", p);
+    });
 }
 
 fn main() {
@@ -177,15 +195,13 @@ configured using the environment variable CODE_DIR.
 Default: ~/Code",
                 ),
         )
-        .subcommand(
-            SubCommand::with_name("list")
-                .arg(Arg::with_name("no-cache").short("n").long("no-cache"))
-                .arg(
-                    Arg::with_name("refresh-cache")
-                        .short("r")
-                        .long("refresh-cache"),
-                ),
+        .arg(Arg::with_name("no-cache").short("n").long("no-cache"))
+        .arg(
+            Arg::with_name("refresh-cache")
+                .short("r")
+                .long("refresh-cache"),
         )
+        .subcommand(SubCommand::with_name("list"))
         .subcommand(
             SubCommand::with_name("run")
                 .setting(AppSettings::TrailingVarArg)
@@ -201,15 +217,14 @@ Default: ~/Code",
         "~/Code".to_string()
     };
 
-    if let Some(args) = matches.subcommand_matches("list") {
-        list(
-            code_dir,
-            args.is_present("no-cache") || args.is_present("refresh-cache"),
-        );
+    let ignore_cache = matches.is_present("no-cache") || matches.is_present("refresh-cache");
+
+    if let Some(_args) = matches.subcommand_matches("list") {
+        list(code_dir, ignore_cache);
     } else if let Some(args) = matches.subcommand_matches("run") {
         let argv: Vec<&str> = args.values_of("ARGV").unwrap().collect();
         let cmd: Vec<String> = argv.iter().map(|x| x.to_string()).collect();
-        run(code_dir, cmd);
+        run(code_dir, ignore_cache, cmd);
     } else {
         println!("ERROR: Unknown command.");
     }
