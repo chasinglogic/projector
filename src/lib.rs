d81bb82514f93ef80a4acbd7f2d9c748f58c9eaf
Mathew Robinson <chasinglogic@gmail.com>
2018-06-25 15:09:57 -0400

    Start refactoring to library

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+extern crate ansi_term;
+extern crate regex;
+extern crate walkdir;
+
+pub mod projects;
--- src/main.rs
+++ src/main.rs
@@ -2,71 +2,18 @@
 // Use of this source code is governed by the GPLv3 license that can be found in
 // the LICENSE file.
 
-extern crate regex;
 extern crate clap;
-extern crate walkdir;
-extern crate ansi_term;
+extern crate projector;
 
+use clap::{App, AppSettings, Arg, SubCommand};
+use projector::projects::find;
 use std::env;
-use clap::{App, Arg, SubCommand, AppSettings};
-use walkdir::WalkDir;
-use std::io::ErrorKind;
-use std::process;
 use std::process::Command;
-
-
-fn handle_error(e: walkdir::Error) -> bool {
-    if let Some(path) = e.path() {
-        if path.is_file() { return true }
-    }
-
-    if let Some(err) = e.io_error() {
-        if err.kind() == ErrorKind::NotFound {
-            return true
-        }
-    }
-
-    println!("ERROR: {}", e);
-    return false
-}
-
-fn find_projects<F>(code_dir: String, callback: F) where F: (Fn(String) -> ()) {
-    let wkd = WalkDir::new(code_dir);
-    let git_dirs = wkd.into_iter();
-
-    for dir in git_dirs {
-        let cwd;
-        match dir {
-            Ok(d) => cwd = d,
-            Err(e) => {
-                if handle_error(e) {
-                    continue
-                } else {
-                    process::exit(1);
-                }
-            },
-        };
-
-        if !cwd.file_type().is_dir() { continue }
-
-        let file_name = cwd.file_name().to_str().unwrap_or("");
-        if file_name == ".git" {
-            let parent_path = cwd
-                .path()
-                .parent()
-                .unwrap()
-                .to_str()
-                .unwrap_or("")
-                .to_string();
-
-            callback(parent_path);
-        }
-    }
-}
 
 fn run(code_dir: String, command: Vec<String>) {
     if let Some((program, arguments)) = command.split_first() {
-        find_projects(code_dir, |p| {
+        find(code_dir, |p| {
+            println!("\n\n{}:", p);
             let mut child = Command::new(program)
                 .args(arguments)
                 .current_dir(p)
@@ -81,27 +28,31 @@ fn run(code_dir: String, command: Vec<String>) {
 }
 
 fn list(code_dir: String) {
-    find_projects(code_dir, |p| println!("{}", p))
+    find(code_dir, |p| println!("{}", p))
 }
 
 fn main() {
     let matches = App::new("projector")
         .version("0.1.0")
         .author("Mathew Robinson <chasinglogic@gmail.com>")
-        .arg(Arg::with_name("code-dir")
-             .short("c")
-             .long("code-dir")
-             .value_name("CODE_DIR")
-             .takes_value(true)
-             .help("The root of where to search for projects. Also can be
+        .arg(
+            Arg::with_name("code-dir")
+                .short("c")
+                .long("code-dir")
+                .value_name("CODE_DIR")
+                .takes_value(true)
+                .help(
+                    "The root of where to search for projects. Also can be
 configured using the environment variable CODE_DIR.
-Default: ~/Code"))
+Default: ~/Code",
+                ),
+        )
         .subcommand(SubCommand::with_name("list"))
-        .subcommand(SubCommand::with_name("run")
-                    .setting(AppSettings::TrailingVarArg)
-                    .arg(Arg::with_name("ARGV")
-                         .multiple(true)
-                         .default_value("")))
+        .subcommand(
+            SubCommand::with_name("run")
+                .setting(AppSettings::TrailingVarArg)
+                .arg(Arg::with_name("ARGV").multiple(true).default_value("")),
+        )
         .get_matches();
 
     let code_dir: String = if let Some(dir) = matches.value_of("code-dir") {
