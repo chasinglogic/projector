261adf84bff117136cda2de13788aa16e5bc8d2c
Mathew Robinson <chasinglogic@gmail.com>
2018-06-25 16:42:09 -0400

    Add support for includes / excludes

--- src/projects.rs
+++ src/projects.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use std::io::ErrorKind;
+use std::process;
 use walkdir;
 use walkdir::WalkDir;
 
@@ -19,11 +21,67 @@ fn skippable_err(e: &walkdir::Error) -> bool {
     return false;
 }
 
-pub fn find<F>(code_dir: String, callback: F)
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub code_dir: String,
+    pub excludes: Option<Vec<String>>,
+    pub includes: Option<Vec<String>>,
+}
+
+pub fn find<F>(config: Config, callback: F)
 where
     F: Fn(String) -> (),
 {
-    let mut wkd = WalkDir::new(code_dir).into_iter();
+    let mut pat;
+    let exclude;
+    let include;
+    if let Some(ref patterns) = config.excludes {
+        pat = "(".to_string();
+
+        for pattern in patterns {
+            pat.push_str(&pattern);
+            pat.push_str("|");
+        }
+
+        // Remove trailing |
+        pat.pop();
+        pat.push_str(")");
+
+        exclude = match Regex::new(&pat) {
+            Ok(r) => r,
+            Err(e) => {
+                println!("ERROR: Unable to compile regex: {}: {}", pat, e);
+                process::exit(1);
+            }
+        }
+    } else {
+        exclude = Regex::new("^$").unwrap();
+    };
+
+    if let Some(ref patterns) = config.includes {
+        pat = "(".to_string();
+
+        for pattern in patterns {
+            pat.push_str(&pattern);
+            pat.push_str("|");
+        }
+
+        // Remove trailing |
+        pat.pop();
+        pat.push_str(")");
+
+        include = match Regex::new(&pat) {
+            Ok(r) => r,
+            Err(e) => {
+                println!("ERROR: Unable to compile regex: {}: {}", pat, e);
+                process::exit(1);
+            }
+        }
+    } else {
+        include = Regex::new("^$").unwrap();
+    };
+
+    let mut wkd = WalkDir::new(config.code_dir).into_iter();
     loop {
         let dir = match wkd.next() {
             None => break,
@@ -36,12 +94,17 @@ where
                 }
             }
         };
-
         let mut path = dir.path().to_path_buf();
         path.push(".git");
         if path.exists() {
-            callback(dir.path().to_str().unwrap_or("").to_string());
             wkd.skip_current_dir();
+
+            let project_path = dir.path().to_str().unwrap_or("").to_string();
+            if exclude.is_match(&project_path) && !include.is_match(&project_path) {
+                continue;
+            }
+
+            callback(project_path);
         }
     }
 }
