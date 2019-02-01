5a67acf85a9d67575e927c88c067df30a5c44ce2
Mathew Robinson <chasinglogic@gmail.com>
2019-02-01 10:08:57 -0500

    Handle broken pipe errors

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,4 +1,6 @@
 use crate::find::projects::Finder;
+use std::io;
+use std::io::Write;
 
 pub const USAGE: &str = "
 Usage: list [options]
@@ -13,11 +15,18 @@ See 'projector help' for more information.
 
 #[derive(Deserialize, Debug)]
 pub struct Args {
-    flag_verbose: bool,
+  flag_verbose: bool,
 }
 
 pub fn run(finder: Finder, _args: &Args) {
-    for project in finder {
-        println!("{}", project.as_os_str().to_string_lossy());
-    }
+  for project in finder {
+    match writeln!(
+      io::stdout().lock(),
+      "{}",
+      project.as_os_str().to_string_lossy()
+    ) {
+      Ok(()) => (),
+      Err(_) => (),
+    };
+  }
 }
